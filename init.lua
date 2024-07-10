b7e6dce04ad9cc98c6085148da71a4a290cc578a
rishabhvoid <rishabhvoid@gmail.com>
2024-07-11 03:36:05 +0800

    Thu July 11

--- init.lua
+++ init.lua
@@ -4,22 +4,53 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
-vim.cmd("set clipboard=unnamed")
-vim.cmd("set rnu")
-vim.opt.termguicolors = true
-vim.g.python_host_prog = "F:\\program_fles\\python_312"
-vim.g.python3_host_prog = "F:\\program_fles\\python_312"
+vim.cmd("set clipboard=unnamedplus")
+vim.opt.scrolloff = 0
+vim.opt.sidescrolloff = 0
+
+-- Enable line numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Lazy.nvim setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+require("leap").create_default_mappings()
+
+-- Remappings
+vim.g.mapleader = " "
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
+vim.cmd("set clipboard=unnamedplus")
+
+-- Enable termguicolors if needed
+-- vim.opt.termguicolors = true
+
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   config = function()
     require("nvim-treesitter.install").compilers = { "clang" }
+    require("nvim-treesitter.install").prefer_git = true
     require("nvim-treesitter.configs").setup {
       ensure_installed = { "c", "vim", "vimdoc", "query" },
       sync_install = false,
