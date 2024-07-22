9ecfc70a882fe83883f4f472e47373799226e427
rishabhvoid <rishabhvoid@gmail.com>
2024-07-22 18:34:13 +0530

    22 July 2024 - Just before trying out arch

--- init.lua
+++ init.lua
@@ -1,14 +1,13 @@
--- Remappings
+-- Settings
 vim.g.mapleader = " "
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
-vim.cmd("set clipboard=unnamedplus")
+vim.opt.clipboard = "unnamedplus"
 vim.opt.scrolloff = 0
 vim.opt.sidescrolloff = 0
-
--- Enable line numbering
+vim.opt.conceallevel = 1
 vim.opt.number = true
 vim.opt.relativenumber = true
 
@@ -20,7 +19,7 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
@@ -29,30 +28,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 require("leap").create_default_mappings()
 
--- Remappings
-vim.g.mapleader = " "
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.bo.softtabstop = 2
-vim.cmd("set clipboard=unnamedplus")
-
--- Enable termguicolors if needed
--- vim.opt.termguicolors = true
-
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
-require("leap").create_default_mappings()
+-- Global shortcuts
+vim.keymap.set("n", "<C-Left>", ":bprev<CR>", {})
+vim.keymap.set("n", "<C-Right>", ":bnext<CR>", {})
